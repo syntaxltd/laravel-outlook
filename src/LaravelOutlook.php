2be1adec5ff166c00bf09d8ae38a45bddedfd182
Dennis Mwea <mweadennis2@gmail.com>
2021-10-25 14:19:32 +0300

    Refresh token

--- src/LaravelOutlook.php
+++ src/LaravelOutlook.php
@@ -2,6 +2,7 @@
 
 namespace Dytechltd\LaravelOutlook;
 
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use League\OAuth2\Client\Provider\GenericProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use Microsoft\Graph\Model\User;
@@ -45,12 +46,55 @@ class LaravelOutlook
 
     public function getAccessToken()
     {
+        // Check if tokens exist
         if (empty(session('accessToken')) ||
             empty(session('refreshToken')) ||
             empty(session('tokenExpires'))) {
             return '';
         }
 
+        // Check if token is expired
+        //Get current time + 5 minutes (to allow for time differences)
+        $now = time() + 300;
+        if (session('tokenExpires') <= $now) {
+            // Token is expired (or very close to it)
+            // so let's refresh
+
+            // Initialize the OAuth client
+            $oauthClient = new GenericProvider([
+                'clientId' => config('azure.appId'),
+                'clientSecret' => config('azure.appSecret'),
+                'redirectUri' => config('azure.redirectUri'),
+                'urlAuthorize' => config('azure.authority') . config('azure.authorizeEndpoint'),
+                'urlAccessToken' => config('azure.authority') . config('azure.tokenEndpoint'),
+                'urlResourceOwnerDetails' => '',
+                'scopes' => config('azure.scopes')
+            ]);
+
+            try {
+                $newToken = $oauthClient->getAccessToken('refresh_token', [
+                    'refresh_token' => session('refreshToken')
+                ]);
+
+                // Store the new values
+                $this->updateTokens($newToken);
+
+                return $newToken->getToken();
+            } catch (IdentityProviderException $e) {
+                return null;
+            }
+        }
+
+        // Token is still valid, just return it
         return session('accessToken');
+    }
+
+    public function updateTokens(AccessToken $accessToken)
+    {
+        session([
+            'accessToken' => $accessToken->getToken(),
+            'refreshToken' => $accessToken->getRefreshToken(),
+            'tokenExpires' => $accessToken->getExpires()
+        ]);
     }
 }
