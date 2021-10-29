ec7382d395755214a2335d39e26df65b13e851ec
Dennis Mwea <mweadennis2@gmail.com>
2021-10-29 15:06:54 +0300

    Add access tokens migration

--- src/LaravelOutlookServiceProvider.php
+++ src/LaravelOutlookServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace Syntax\LaravelOutlook;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use function Safe\date;
 
 class LaravelOutlookServiceProvider extends ServiceProvider
 {
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected string $namespace = 'Syntax\Http\Controller;s';
+
     /**
      * Bootstrap any package services.
      *
@@ -16,7 +27,15 @@ class LaravelOutlookServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/azure.php' => config_path('azure.php'),
         ]);
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        if ($this->app->runningInConsole()) {
+            // Export the migration
+            if (!class_exists('CreateSocialAccessTokensTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_social_access_tokens_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_social_access_tokens.php'),
+                ], 'migrations ');
+            }
+        }
     }
 
     /**
@@ -29,5 +48,20 @@ class LaravelOutlookServiceProvider extends ServiceProvider
         $this->app->bind('laravel-outlook', function ($app) {
             return new LaravelOutlook();
         });
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the package.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(__DIR__ . '/../routes/web.php');
     }
 }
