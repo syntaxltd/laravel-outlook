3d828369914a832a19c57cec839ca3fab99e11d5
eva <evamwng@gmail.com>
2021-11-04 08:06:52 +0300

    Updates

--- src/LaravelGmail.php
+++ src/LaravelGmail.php
@@ -3,8 +3,10 @@
 
 namespace Dytechltd\LaravelOutlook;
 
+use Dytechltd\LaravelOutlook\Traits\Configurable;
 use Google_Client;
 use Google_Service_Gmail;
+use Illuminate\Support\Facades\Storage;
 use League\Flysystem\Exception;
 use \Safe\file_get_contents;
 use \Safe\json_decode;
@@ -12,60 +14,108 @@ use \Safe\json_decode;
 class LaravelGmail extends Google_Client
 {
 
-    /**
-     * @throws \Google\Exception
-     */
-    public function getOAuthClient(): Google_Client
-    {
-        $this->setApplicationName('Gmail API PHP Quickstart');
-        $this->setScopes(Google_Service_Gmail::GMAIL_READONLY);
-        $this->setAuthConfig('credentials.json');
-        $this->setAccessType('offline');
-        $this->setPrompt('select_account consent');
+    use Configurable {
+        Configurable::__construct as configConstruct;
+    }
+    protected $emailAddress;
+    protected $refreshToken;
+    protected $app;
+    protected $accessToken;
+    protected $token;
+    protected $userId;
 
-        return $this;
+    public function __construct(array $config = array(), $userId = null)
+    {
+
+        $this->userId = $userId;
+
+        $this->configConstruct($config);
+
+        parent::__construct($this->getConfigs());
+
+        $this->configApi();
+
+        if ($this->checkPreviouslyLoggedIn()) {
+            $this->refreshTokenIfNeeded();
+        }
+
     }
 
-    public function getAccessToken(): string
+    /**
+     * Gets the URL to authorize the user
+     *
+     * @return string
+     */
+    public function getOAuthClient(): string
     {
-        // Load previously authorized token from a file, if it exists.
-        // The file token.json stores the user's access and refresh tokens, and is
-        // created automatically when the authorization flow completes for the first
-        // time.
-        $tokenPath = 'token.json';
-        if (file_exists($tokenPath)) {
-            $accessToken = json_decode(file_get_contents($tokenPath), true);
-            $this->setAccessToken($accessToken);
+        return $this->createAuthUrl();
+    }
+
+    /**
+     * Check and return true if the user has previously logged in without checking if the token needs to refresh
+     *
+     * @return bool
+     */
+    public function checkPreviouslyLoggedIn(): bool
+    {
+
+        return !$this->config();
+    }
+
+    /**
+     * Refresh the auth token if needed
+     *
+     * @return mixed|null
+     */
+    private function refreshTokenIfNeeded(): mixed
+    {
+        if ($this->isAccessTokenExpired()) {
+            $this->fetchAccessTokenWithRefreshToken($this->getRefreshToken());
+            $token = $this->getAccessToken();
+            parent::setAccessToken($token);
+            $this->saveAccessToken($token);
+
+            return $token;
         }
 
-        // If there is no previous token or it's expired.
-        if ($this->isAccessTokenExpired()) {
-            // Refresh the token if possible, else fetch a new one.
-            if ($this->getRefreshToken()) {
-                $this->fetchAccessTokenWithRefreshToken($this->getRefreshToken());
-            } else {
-                // Request authorization from the user.
-                $authUrl = $this->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
+        return $this->token;
+    }
 
-                // Exchange authorization code for an access token.
-                $accessToken = $this->fetchAccessTokenWithAuthCode($authCode);
-                $this->setAccessToken($accessToken);
 
-                // Check to see if there was an error.
-                if (array_key_exists('error', $accessToken)) {
-                    throw new Exception(join(', ', $accessToken));
+    /**
+     * Save the credentials in a file
+     *
+     * @param  array  $config
+     */
+    public function saveAccessToken(array $config)
+    {
+        $disk = Storage::disk('local');
+        $fileName = config('credentials_file_name');
+        $file = "gmail/tokens/$fileName.json";
+        $allowJsonEncrypt = config('gmail.allow_json_encrypt');
+        $config['email'] = $this->emailAddress;
+
+        if ($disk->exists($file)) {
+
+            if (empty($config['email'])) {
+                if ($allowJsonEncrypt) {
+                    $savedConfigToken = json_decode(decrypt($disk->get($file)), true);
+                } else {
+                    $savedConfigToken = json_decode($disk->get($file), true);
+                }
+                if(isset( $savedConfigToken['email'])) {
+                    $config['email'] = $savedConfigToken['email'];
                 }
             }
-            // Save the token to a file.
-            if (!file_exists(dirname($tokenPath))) {
-                mkdir(dirname($tokenPath), 0700, true);
-            }
-            file_put_contents($tokenPath, json_encode($this->getAccessToken()));
+
+            $disk->delete($file);
         }
 
-        return $accessToken;
+        if ($allowJsonEncrypt) {
+            $disk->put($file, encrypt(json_encode($config)));
+        } else {
+            $disk->put($file, json_encode($config));
+        }
+
     }
 }
