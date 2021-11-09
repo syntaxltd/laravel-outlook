dbf89ec5ec0249558e7f6a760a1976f11d6e8f42
evamwng <evamwng@gmail.com>
2021-11-09 13:29:40 +0300

    updates

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -31,51 +31,10 @@ class LoginController extends Controller
     /**
      * @throws Throwable
      */
-    public function callback(Request $request): Redirector|Application|RedirectResponse
+    public function callback(Request $request, string $client): Redirector|Application|RedirectResponse
     {
-        // Validate the state
-        $expectedState = session('oauthState');
-        $request->session()->forget('oauthState');
-        $providedState = $request->query('state');
-
-        if (!isset($expectedState)) {
-            // If there is no expected state in the session,
-            // do nothing and redirect to the home page.
-            return redirect('/test');
-        }
-
-        throw_if(!isset($providedState) || $expectedState != $providedState, new InvalidStateException());
-
-        // Authorization code should be in the "code" query param
-        $authCode = $request->query('code');
-        if (isset($authCode)) {
-            try {
-                /**
-                 * @var AccessToken $accessToken
-                 */
-                $accessToken = app('laravel-outlook')->getOAuthClient()->getAccessToken('authorization_code', [
-                    'code' => $authCode,
-                ]);
-
-                $graph = (new Graph)->setAccessToken($accessToken->getToken());
-
-                $user = $graph->createRequest('GET', '/me?$select=displayName,mail,mailboxSettings,userPrincipalName')
-                    ->setReturnType(User::class)
-                    ->execute();
-
-                app('laravel-outlook')->storeTokens($accessToken, $user);
-
-                return redirect('/');
-            } catch (IdentityProviderException $exception) {
-                return redirect('/')
-                    ->with('error', 'Error requesting access token')
-                    ->with('errorDetail', json_encode($exception->getResponseBody()));
-            }
-        }
-
-        return redirect('/')
-            ->with('error', $request->query('error'))
-            ->with('errorDetail', $request->query('error_description'));
+        LaravelSocialIntegration::service($client)->auth()->storeToken($request);
+        return redirect('/');
     }
 
     public function logout(): Redirector|Application|RedirectResponse
