d1286bd5dbe8fdefae67fc88e131a27f45f62447
Dennis Mwea <mweadennis2@gmail.com>
2021-11-16 16:27:47 +0300

    Refactor code

--- src/Modules/gmail/LaravelGmail.php
+++ src/Modules/gmail/LaravelGmail.php
@@ -3,11 +3,13 @@
 
 namespace Syntax\LaravelSocialIntegration\Modules\gmail;
 
+use App\Models\PartnerUser;
 use Google_Service_Gmail_Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Syntax\LaravelSocialIntegration\Contracts\SocialClient;
-use Syntax\LaravelSocialIntegration\Models\SocialAccessEmail;
+use Syntax\LaravelSocialIntegration\Models\SocialAccessMail;
 use Syntax\LaravelSocialIntegration\Models\SocialAccessToken;
 use Syntax\LaravelSocialIntegration\Modules\gmail\services\GmailConnection;
 use Syntax\LaravelSocialIntegration\Modules\gmail\services\Mail;
@@ -30,14 +32,11 @@ class LaravelGmail extends GmailConnection implements SocialClient
         return $this->service->users_messages->get('me', $id);
     }
 
-    /**
-     * @return mixed
-     */
-    public function all(): mixed
+    public function all(): Collection
     {
-        $client = SocialAccessToken::where('partner_user_id', Auth::id())->where('type', 'gmail')->pluck('id');
-
-        return SocialAccessEmail::whereIn('social_access_token_id', $client)->get();
+        $client = SocialAccessToken::query()->where('partner_user_id', Auth::id())->where('type', 'gmail')->pluck('id');
+        
+        return SocialAccessMail::query()->whereIn('token_id', $client)->get();
     }
 
     /**
@@ -49,17 +48,22 @@ class LaravelGmail extends GmailConnection implements SocialClient
      */
     public function send(Request $request): Mail
     {
+        /** @var PartnerUser $user */
+        $user = auth('partneruser')->user();
+
         $mail = new Mail();
         $mail->to($this->getContacts($request));
-        $mail->from(Auth::user()->email, Auth::user()->name);
+        $mail->from($user->email, $user->name);
         $mail->cc($request->input('cc'));
         $mail->bcc($request->input('bcc'));
         $mail->subject($request->input('subject'));
         $mail->message($request->input('message'));
-        $mail->attach($request->input('attachments'));
+
+        if (!is_null($request->input('attachments'))) {
+            $mail->attach($request->input('attachments'));
+        }
         $mail->send();
 
-        $this->storeEmail($mail);
         return $mail;
     }
 
@@ -68,21 +72,5 @@ class LaravelGmail extends GmailConnection implements SocialClient
         return collect($request->input('contact'))->filter()->map(function ($item) {
             return $item['email'];
         })->toArray();
-    }
-
-    /**
-     * @param Mail $mail
-     * @return SocialAccessEmail
-     */
-    private function storeEmail(Mail $mail): SocialAccessEmail
-    {
-        $email = new SocialAccessEmail;
-        $email->email_id = $mail->id;
-        $email->thread_id = $mail->threadId;
-        $email->social_access_token_id = SocialAccessToken::Where('partner_user_id', Auth::id())->where('type', 'gmail')->first()->id;
-        $email->data = json_encode($mail);
-        $email->save();
-
-        return $email->refresh();
     }
 }
