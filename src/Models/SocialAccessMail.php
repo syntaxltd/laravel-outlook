998a1c6486e129cc17a87cea2e2a4ba94f212a96
evamwng <evamwng@gmail.com>
2021-11-22 09:48:34 +0300

    Add email delete function

--- src/Models/SocialAccessMail.php
+++ src/Models/SocialAccessMail.php
@@ -9,6 +9,7 @@ use App\Models\Deal;
 use App\Models\Property;
 use App\Traits\HasAttachment;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Http\Request;
@@ -111,5 +112,13 @@ class SocialAccessMail extends Model
     public function deals(): MorphToMany
     {
         return $this->morphedByMany(Deal::class, 'social_access_mailable');
+    }
+
+    /**
+     * Get the email provider access token.
+     */
+    public function provider(): BelongsTo
+    {
+        return $this->belongsTo(SocialAccessToken::class,'token_id');
     }
 }
